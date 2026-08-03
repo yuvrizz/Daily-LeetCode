cd48169c0e4e48ae069392044919ba3297106ccb
Yuvraj Singh <uvyuvrajsingh25@gmail.com>
2026-08-03 10:25:35 +0530

    Time: 5 ms (70.82%), Space: 15.4 MB (54.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,42 +1,98 @@
+// class Solution {
+// public:
+//     int helper(vector<int>& nums, vector<vector<int>> &dp, int idx, int target, int offset){
+
+//         if(idx == nums.size()){
+//             return target == 0;
+//         }
+
+//         if (target > offset || target < -offset){
+//             return 0;
+//         }
+
+//         int shifted = target + offset;
+
+//         if(dp[idx][shifted] != -1){
+//             return dp[idx][shifted];
+//         }
+
+//         int plus = helper(nums,dp,idx+1,target - nums[idx],offset);
+//         int minus = helper(nums,dp,idx+1,target + nums[idx],offset);
+
+//         dp[idx][shifted] = plus + minus;
+
+//         return dp[idx][shifted];
+//     }
+
+//     int findTargetSumWays(vector<int>& nums, int target) {
+        
+//         int n = nums.size();
+//         int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+//         if(abs(target) > totalSum){
+//             return 0;
+//         }
+
+//         int offset = totalSum;
+
+//         vector<vector<int>> dp(n,vector<int>(2*totalSum + 1,-1));
+
+//         return helper(nums,dp,0,target,offset);
+//     }
+// };
+
+
+// Partition sum concept.
+
+
 class Solution {
 public:
-    int helper(vector<int>& nums, vector<vector<int>> &dp, int idx, int target, int offset){
+    int helper(vector<int>& nums, vector<vector<int>>& dp, int idx, int sum){
 
-        if(idx == nums.size()){
-            return target == 0;
-        }
-
-        if (target > offset || target < -offset){
+        if(idx == 0){
+            if(sum == 0 && nums[0] == 0){
+                return 2;
+            }    
+            if(sum == 0 || sum == nums[0]){
+                return 1;
+            }     
             return 0;
         }
 
-        int shifted = target + offset;
-
-        if(dp[idx][shifted] != -1){
-            return dp[idx][shifted];
+        if(dp[idx][sum] != -1){
+            return dp[idx][sum];
         }
+            
+        int notTake = helper(nums, dp, idx-1, sum);
 
-        int plus = helper(nums,dp,idx+1,target - nums[idx],offset);
-        int minus = helper(nums,dp,idx+1,target + nums[idx],offset);
+        int take = 0;
 
-        dp[idx][shifted] = plus + minus;
-
-        return dp[idx][shifted];
+        if(nums[idx] <= sum){
+            take = helper(nums, dp, idx-1, sum-nums[idx]);
+        }
+        
+        return dp[idx][sum] = take + notTake;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        int n = nums.size();
+
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
 
-        if(abs(target) > totalSum){
+        if (abs(target) > totalSum){
             return 0;
         }
 
-        int offset = totalSum;
+        if ((totalSum + target) % 2 != 0){
+            return 0;
+        }
+            
 
-        vector<vector<int>> dp(n,vector<int>(2*totalSum + 1,-1));
+        int reqSum = (totalSum + target) / 2;
 
-        return helper(nums,dp,0,target,offset);
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(reqSum + 1, -1));
+
+        return helper(nums, dp, n - 1, reqSum);
     }
 };

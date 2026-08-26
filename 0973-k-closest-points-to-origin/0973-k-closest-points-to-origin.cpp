eb4030b1c18b86adef18469888d97915e0871b9a
Yuvraj Singh <uvyuvrajsingh25@gmail.com>
2026-08-27 00:04:29 +0530

    Time: 78 ms (35.77%), Space: 77.9 MB (36.7%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,8 +1,49 @@
+// Not optimized TC
+// because each insertion costs O(logn) TC, so overall it becomes (nlogn) for n elements.
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+//         priority_queue <pair<int,vector<int>>, vector<pair<int,vector<int>>>, greater<pair<int,vector<int>>>> pq;
+
+//         for(int i=0; i<points.size(); i++){
+
+//             int x = points[i][0];
+//             int y = points[i][1];
+
+//             int dist = x*x + y*y;
+//             pq.push({dist,points[i]});
+//         }
+
+//         vector<vector<int>> ans;
+
+//         for(int i=0; i<k; i++){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+
+//         return ans;
+//     }
+// };
+
+// -----------------------------------------------------------------------------
+
+// Optimal TC
+// Max heap of size k - O(n logk)
+
+// Calculating distances     O(n)
+// n pushes                  O(n log k)
+// up to n pops              O(n log k)
+// ------------------------------------
+// Total                     O(n log k)
+
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
-        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int, vector<int>>>> pq;
+        priority_queue <pair<int,vector<int>>> pq;
 
         for(int i=0; i<points.size(); i++){
 
@@ -10,12 +51,17 @@ public:
             int y = points[i][1];
 
             int dist = x*x + y*y;
+
             pq.push({dist,points[i]});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
         }
 
         vector<vector<int>> ans;
 
-        for(int i=0; i<k; i++){
+        while(!pq.empty()){
             ans.push_back(pq.top().second);
             pq.pop();
         }

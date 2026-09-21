23827d6caa88bc8b55e243a050e0721cf9e0b738
Yuvraj Singh <uvyuvrajsingh25@gmail.com>
2026-09-21 15:09:28 +0530

    Time: 3 ms (32.41%), Space: 14 MB (74.93%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        
+        int n = arr.size();
+        int i = 0;
+
+        while(i<n){
+            if(arr[i] == 0){
+                arr.insert(arr.begin()+i+1,0);
+                arr.pop_back();
+                i++;
+            }
+            i++;
+        }
+
+    }
+};
